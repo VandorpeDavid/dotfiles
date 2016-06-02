941448d0488a8f2f214b90289ef593b195a96cd0
David Vandorpe <david.vandorpe@ugent.be>
2016-06-02 03:39:52 +0200

    Desktop notifications for puush

--- scripts/screen.sh
+++ scripts/screen.sh
@@ -1,3 +1,3 @@
 FILE=$(mktemp --suffix=".png")
-escrotum -s $FILE --exec "puush $FILE | grep 'http' | xclip -selection clipboard"
+escrotum -s $FILE --exec "puush $FILE | grep 'http' | xclip -selection clipboard && notify-send Uploaded -a Puush"
 rm $FILE
