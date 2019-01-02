6715c1f0d340393eede1e77350ad2da5f7e56627
David Vandorpe <david.vandorpe2@gmail.com>
2019-01-02 21:16:49 +0100

    No need for sudo here

--- scripts/hdmi_toggle.sh
+++ scripts/hdmi_toggle.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-USER_NAME=$(whoami)
-USER_ID=$(id -u "$USER_NAME")
-PULSE_SERVER="unix:/run/user/"$USER_ID"/pulse/native"
 
-if sudo -u "$USER_NAME" pactl --server "$PULSE_SERVER" info | grep --quiet hdmi
+if pactl info | grep --quiet hdmi
 then
     echo disconnect
-    sudo -u "$USER_NAME" pactl --server "$PULSE_SERVER" set-card-profile 0 output:analog-stereo+input:analog-stereo
+    pactl set-card-profile 0 output:analog-stereo+input:analog-stereo
 else
-    sudo -u "$USER_NAME" pactl --server "$PULSE_SERVER" set-card-profile 0 output:hdmi-stereo+input:analog-stereo
+    pactl set-card-profile 0 output:hdmi-stereo+input:analog-stereo
     echo connect
 fi
 
